7dc1779ecfd429435cc45cf0b677657721ddc66b
DreadfulBot <therawlex@gmail.com>
2018-05-07 13:56:44 +0500

    fixed prod mode

--- git/git-add.py
+++ git/git-add.py
@@ -1,11 +1,15 @@
 import subprocess
 import sys
+import os
+
+source = os.path.dirname(__file__)
+parent = os.path.join(source, '../')
 
 if(len(sys.argv) < 2):
     comment = "fixes"
 else:
     comment = sys.argv[1]
 
+subprocess.call("git add .", shell=True, cwd=parent)
 subprocess.call(f"git commit -m \"{comment:s}\"")
-subprocess.call("cd ..&&git add .")
 subprocess.call("git push origin master")
